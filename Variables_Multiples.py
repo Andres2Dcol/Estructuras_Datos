f06145e1c36d48ef297da13490ca09863990556c
Andres2Dcol <167625465+Andres2Dcol@users.noreply.github.com>
2024-09-18 15:51:15 -0500

    4_Variables_Multiples

---
+++ Variables_Multiples.py
@@ -0,0 +1,25 @@
+j,m,g = "Juan", "German", "Teresa"
+
+print(j)
+print(m)
+print(g)
+
+#Es interesante ver como se puede nombrar de una manera tan rapida las varibles en Python
+
+x = y = w = "Azabache"
+
+print(x)
+print(y)
+print(w)
+
+# Igualmente ver como 3 variables pueden tomar el mismo valor o informacion facilitando algunos proscesos
+
+equipos = ["Barcelona ", "Madrid ", "Paris"]
+
+b,m,p = equipos
+
+print(b)
+print(m)
+print(p)
+
+#aqui podemos guardar los valores de una lista y asignarles rapidamente los valores de una manera ordenada |
