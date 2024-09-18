282743403b278b6bc185f2b51df657e5ba72237c
Andres2Dcol <167625465+Andres2Dcol@users.noreply.github.com>
2024-09-18 15:25:58 -0500

    2_Variables

---
+++ Variables.py
@@ -0,0 +1,17 @@
+a = "Andres"
+b = 2005
+
+print (a)
+print (b)
+
+c = str(3) # se va a guardar e imprmir como texto
+d = int(3) # se va a gusrdar como varialble tipo entero 
+e = float(3) # se va guardar como una variabel decimal entera 
+
+print (c) 
+print (d) 
+print (e) 
+
+print(type(c))
+print(type(d))
+print(type(e))
