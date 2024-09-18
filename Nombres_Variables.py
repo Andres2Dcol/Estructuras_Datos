1772890fc232e568c139756be4202a2be2c351a7
Andres2Dcol <167625465+Andres2Dcol@users.noreply.github.com>
2024-09-18 15:37:55 -0500

    3__Nombres_Variables

---
+++ Nombres_Variables.py
@@ -0,0 +1,11 @@
+varia = "Santiago"
+vaRia = "Santiago 2"
+Varia = "Santigo 3" 
+
+print(varia)
+print(vaRia)
+print(Varia)
+ 
+# no importa si las letras esten en matusculas o en minusculas cada variable es unica 
+# pero no pueden tener el mismo tamaño 
+
