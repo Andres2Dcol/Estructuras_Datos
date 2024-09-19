3b8e2e51025de64093a14bab8bb3e7c02b1d2ea1
Andres2Dcol <167625465+Andres2Dcol@users.noreply.github.com>
2024-09-19 14:23:14 -0500

    5_Salida de Variables

---
+++ Salida_Variables.py
@@ -0,0 +1,24 @@
+#maneras de imprimir variables
+
+a = "Feliz Jueves"
+
+print (a)
+
+b = "Hoy "
+c =  "es"
+d = "Jueves"
+
+print(b,c,d)
+
+#maneras de imprimir multiples varibles de una manera mucho mas rapida
+# cabe aclarar que al momento de imprimir las variables tambien se puede separar con el simbolo (+)
+
+#x = 17
+#y = "Santiago"
+# print(x + y)
+# al momento de imprimir varialbes de diferente tipo lo mejor es separarlo con comas ya que nos va a dar error
+
+x = 17
+y = "Santiago"
+
+print(x,y)
