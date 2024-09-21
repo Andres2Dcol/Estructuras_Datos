b66c8688367408d85d667660cb40882e273ddacf
Andres2Dcol <167625465+Andres2Dcol@users.noreply.github.com>
2024-09-21 15:51:37 -0500

    8_Tipos_de_Variable

---
+++ Tipos_de_Variable.py
@@ -0,0 +1,61 @@
+a = "Hola , como tas" #variable tipo texto
+b = 17 #variable que va a tomar topo de enteros
+c = 17.5 #variable que va a toamr variables enteros
+d = 1j   #variables de tipo complejo
+e = ["Leche", "Pan", "Carne"] # variable la cual va a guardar una 
+f = ("Leche", "Pan", "Carne") # variable tupla o tuple la cual guarda una lista inmutable(que su orden no va a cambiar)
+g = range(5) #variable la cual va a guardar un rango determinado de Datos
+h = {"nombre":"Santiago" , "Edad": 17 } #variable la cual va a guardar datos accediendo a ella con una clave
+i = {"Tierra", "Marte", "Jupiter"}# variable tipo set la cual va a guardar datos de manera no repetida
+j = frozenset({"Guanabana","Pitalla","Papaya"})# son varibles de un objeto que son inmutables 
+k = True #la variable puede tomar valores o falsos 
+l = b"huevos" #variable tipo byte que va almacenar un dato
+m = bytearray(6) #es un vector que va a guardar valores tipo byte
+n = memoryview(bytes(6)) # es el tamanio de memoria que va a guardar un array de bytes
+o = x = None # variables que guardan un tipo de variable desconocido
+
+print(a)
+print(type(a)) 
+
+print(b)
+print(type(b)) 
+
+print(c)
+print(type(c)) 
+
+print(d)
+print(type(d)) 
+
+print(e)
+print(type(e)) 
+
+print(f)
+print(type(f)) 
+
+print(g)
+print(type(g)) 
+
+print(h)
+print(type(h)) 
+
+print(i)
+print(type(i)) 
+
+print(j)
+print(type(j))
+
+print(k)
+print(type(k)) 
+
+print(l)
+print(type(l)) 
+
+print(m)
+print(type(m)) 
+
+print(n)
+print(type(n)) 
+
+print(o)
+print(type(o)) 
+
