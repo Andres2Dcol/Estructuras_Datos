02a0eaf6cbc5e26e6a0893a55d782022442c3502
Andres2Dcol <167625465+Andres2Dcol@users.noreply.github.com>
2024-09-21 16:15:45 -0500

    7_Numeros_Python

---
+++ Python_Numeros.py
@@ -0,0 +1,23 @@
+a = 17 #tipo entero
+b = 5.5 #tipo decimal (float)
+c = 8j #tipo complejo 
+
+#imprimir el tipo de numero\
+print(type(a))
+print(type(b))
+print(type(c))
+
+#para numeros float tambien se puede tomar como exponencial
+
+d = 12E4
+print(type(d))
+
+print(a)
+print(b)
+print(c)
+
+#Numeros Random
+
+import random #se importa la libreria random
+
+print(random.randrange(1, 100)) 
