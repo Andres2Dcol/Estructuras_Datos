11d29e4e7ab9dab0a84f8c012b0cf1848490da12
Andres2Dcol <167625465+Andres2Dcol@users.noreply.github.com>
2024-09-22 14:13:49 -0500

    14_Formatos_String

---
+++ Formatos_String.py
@@ -0,0 +1,15 @@
+#agregar datos tipo enteros o float a un string
+
+titulos = 5
+a = f"El barcelona tiene {titulos} titulos de champions" #la f tiene la funcion de cuerda para agregar al texto una variable tipo entero o float
+print(a)
+
+#para mostrar con numeros decimales un enteros se le agrega la cantidad de numeros decimales con ":.nf" donde n representa la cantidad de decimales a mostrar
+b = f"El barcelona tiene {titulos:.3f} titulos de champions" #la f tiene la funcion de cuerda para agregar al texto una variable tipo entero o float
+print(b)
+
+#para hacer operaciones matematicas se realizan denntro de su respectivo corchete {}
+
+c = f"El barcelona tiene {5*1} titulos de champions" #la f tiene la funcion de cuerda para agregar al texto una variable tipo entero o float
+print(c)
+
