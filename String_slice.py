5f603112775e819e2661b9d2a1a803d8474bfdd0
Andres2Dcol <167625465+Andres2Dcol@users.noreply.github.com>
2024-09-22 10:45:38 -0500

    11_String_Slice

---
+++ String_slice.py
@@ -0,0 +1,14 @@
+# vamos a rebanar en python , para hacerlo podemos seleccionar de que poscision a que poscision de una cadena de texto vamos a imprimir
+
+a= "Hoy es Domingo"
+print(a[7:14])
+
+#rebanar desde el principo
+b= "Hoy es Domingo"
+print(b[:3])
+
+#rebanar hasta el final
+
+c= "Hoy es Domingo"
+print(c[4:])
+
