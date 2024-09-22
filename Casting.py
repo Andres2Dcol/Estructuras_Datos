f2e270f03ff7af839a362a46a26256c56e03dfc8
Andres2Dcol <167625465+Andres2Dcol@users.noreply.github.com>
2024-09-22 10:15:49 -0500

    9_Casting

---
+++ Casting.py
@@ -0,0 +1,13 @@
+#Se puede especificar cada tipo de dato de una variable
+
+a = int(17)   # a va a tomar el valor entero 17
+
+b = float(7.7)   # b va a tomar un valor decimal de 7.7
+
+c = str(17.0)  #c va tomara un dato tipo texto que correstponde a "17.0"
+
+print(a)
+
+print(b)
+
+print(c)
