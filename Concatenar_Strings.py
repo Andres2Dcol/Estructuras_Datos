f8ef1c1cd4ce7acff10819235ddc75aa54e0f8cc
Andres2Dcol <167625465+Andres2Dcol@users.noreply.github.com>
2024-09-22 13:59:58 -0500

    13_Conacatenar_Strings

---
+++ Concatenar_Strings.py
@@ -0,0 +1,16 @@
+# vamos a concatenar cadenas para formar una solamente o mas
+
+a =  "Visca"
+b = "Barca"
+#la variable c va a ser la encagada de sumar ambos strings y dejarlos en una sola cadena de texto
+
+c = a+b
+
+print(c)
+
+#ahora vamos a dejar un espacio entre ellas 2
+
+d = a + " " + b
+
+
+
